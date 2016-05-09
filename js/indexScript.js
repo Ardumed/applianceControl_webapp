7c92307863753ffd3bb1f7589ff57191a5d8b905
Himanshu Singh <himanshusingh2407@gmail.com>
2016-05-09 23:07:25 +0530

    added toggling switches after data refreshing

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,12 +1,41 @@
 $(document).ready(function(){
-    $(".toggle").click(function(){
-        if($(this).children("input").is(":checked"))
-        $(this).parent().parent().parent().addClass("focus-layout-active");
-      else
-        $(this).parent().parent().parent().removeClass("focus-layout-active");
-      $( "#submit" ).trigger( "click" );
+  goToByScroll("switch-container");
+  $(".toggle").click(function(){
+    if($(this).children("input").is(":checked"))
+      $(this).parent().parent().parent().addClass("focus-layout-active");
+    else
+      $(this).parent().parent().parent().removeClass("focus-layout-active");
+    $( "#submit" ).trigger( "click" );
+  });
+
+  $('#submit').click(function() {
+    return true;
+  });
+
+  $.get( "currentstatus", function( data ) {
+//    console.log(data);
+    $.each(data, function (index, value) {
+      if(index != '_id'){
+        var x= '[name="'+index+'"]';
+        $(x).prop('checked', value);
+        console.log(x,value);
+      }
     });
-    $('#submit').click(function() {
-        return false;
-    });
+  });
+var upd = setTimeout(update,5000);
+  function update(){
+    $("input[type=checkbox]").each(function(){
+//    console.log(this,$(this).is(":checked"));
+    if($(this).is(":checked"))
+      $(this).parent().parent().parent().parent().addClass("focus-layout-active");
+    else
+      $(this).parent().parent().parent().parent().removeClass("focus-layout-active");
+  });
+    }
+
+  function goToByScroll(id){
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top},
+        'slow');
+}
 });
