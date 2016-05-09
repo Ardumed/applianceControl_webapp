d3a452c3d3135550be878ffcf760da56ba3e09a3
Himanshu Singh <himanshusingh2407@gmail.com>
2016-05-09 17:27:14 +0530

    added auto submit and toggle box status

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     $(".toggle").click(function(){
-//        if($(this).children("input").val()){}
-      console.log($(this).children("input"));
+        if($(this).children("input").is(":checked"))
         $(this).parent().parent().parent().addClass("focus-layout-active");
-        $(this).parent().parent().parent().addClass("focus-layout-active");
+      else
+        $(this).parent().parent().parent().removeClass("focus-layout-active");
+      $( "#submit" ).trigger( "click" );
+    });
+    $('#submit').click(function() {
+        return false;
     });
 });
