7cca1c6de11c7a32d28a438f6a1413247902b1fc
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-05-10 17:59:23 +0530

    sanitized to deploy on heroku

--- js/indexScript.js
+++ js/indexScript.js
@@ -5,23 +5,23 @@ $(document).ready(function() {
             $(this).parent().parent().parent().addClass("focus-layout-active");
         else
             $(this).parent().parent().parent().removeClass("focus-layout-active");
-        $("#submit").trigger("click");
+        // $("#submit").trigger("click");
     });
 
     $('#submit').click(function() {
         return true;
     });
 
-    $.get("currentstatus", function(data) {
-        //    console.log(data);
-        $.each(data, function(index, value) {
-            if (index != '_id') {
-                var x = '[name="' + index + '"]';
-                $(x).prop('checked', value);
-                console.log(x, value);
-            }
-        });
-    });
+    // $.get("currentstatus", function(data) {
+    //     //    console.log(data);
+    //     $.each(data, function(index, value) {
+    //         if (index != '_id') {
+    //             var x = '[name="' + index + '"]';
+    //             $(x).prop('checked', value);
+    //             console.log(x, value);
+    //         }
+    //     });
+    // });
     var upd = setTimeout(update, 100);
     // update();
 
