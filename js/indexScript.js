72ca43c3508671b5a56ef06ff9a8356f92b6414d
vatsalya25 <vatsalya25@gmail.com>
2016-05-10 17:14:55 +0530

    login page added

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,41 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
   goToByScroll("switch-container");
-  $(".toggle").click(function(){
-    if($(this).children("input").is(":checked"))
+  $(".toggle").click(function() {
+    if ($(this).children("input").is(":checked"))
       $(this).parent().parent().parent().addClass("focus-layout-active");
     else
       $(this).parent().parent().parent().removeClass("focus-layout-active");
-    $( "#submit" ).trigger( "click" );
+    $("#submit").trigger("click");
   });
 
   $('#submit').click(function() {
     return true;
   });
 
-  $.get( "currentstatus", function( data ) {
-//    console.log(data);
-    $.each(data, function (index, value) {
-      if(index != '_id'){
-        var x= '[name="'+index+'"]';
+  $.get("currentstatus", function(data) {
+    //    console.log(data);
+    $.each(data, function(index, value) {
+      if (index != '_id') {
+        var x = '[name="' + index + '"]';
         $(x).prop('checked', value);
-        console.log(x,value);
+        console.log(x, value);
       }
     });
   });
-var upd = setTimeout(update,5000);
-  function update(){
-    $("input[type=checkbox]").each(function(){
-//    console.log(this,$(this).is(":checked"));
-    if($(this).is(":checked"))
-      $(this).parent().parent().parent().parent().addClass("focus-layout-active");
-    else
-      $(this).parent().parent().parent().parent().removeClass("focus-layout-active");
-  });
-    }
+  var upd = setTimeout(update, 1000);
 
-  function goToByScroll(id){
+  function update() {
+    $("input[type=checkbox]").each(function() {
+      //    console.log(this,$(this).is(":checked"));
+      if ($(this).is(":checked"))
+        $(this).parent().parent().parent().parent().addClass("focus-layout-active");
+      else
+        $(this).parent().parent().parent().parent().removeClass("focus-layout-active");
+    });
+  }
+
+  function goToByScroll(id) {
     $('html,body').animate({
-        scrollTop: $("#"+id).offset().top},
-        'slow');
-}
+      scrollTop: $("#" + id).offset().top
+    },1);
+  }
 });
